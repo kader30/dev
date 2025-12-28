340de42d04f047b2a88058c616020192cc93220a
Abdelkader Bensadi <abdelkaderbensadi@gmail.com>
2025-12-29 00:23:48 +0100

    finaliser le projet avec toutes les fonctionalité

--- src/Assets/Components/Footer.jsx
+++ src/Assets/Components/Footer.jsx
@@ -1,47 +1,48 @@
+import { NavLink } from "react-router-dom";
 export default function Footer() {
-  return (
-    <footer className="bg-dark text-white pt-4 px-5">
-      <div className="container">
-        <div className="row">
+    return (
+        <footer className="bg-dark text-white pt-4 px-5">
+            <div className="container">
+                <div className="row">
 
-          <adress className="col-md-4">
-            <h5>John Doe</h5>
-            <p>
-              40 rue Laure Diebold<br />
-              69009 Lyon, France<br />
-              10 20 30 40 50<br />
-              john.doe@gmail.com
-            </p>
-             <i className="bi bi-github mx-2 fs-3 text-secondary "></i>
-                 <i className="bi bi-twitter mx-2 fs-3 text-secondary"></i>
-                <i className="bi bi-linkedin mx-2 fs-3 text-secondary"></i>
-          </adress>
+                    <address className="col-md-4">
+                        <p className="fs-4 fw-bold">John Doe</p>
+                        <p>
+                            40 rue Laure Diebold<br />
+                            69009 Lyon, France<br />
+                            10 20 30 40 50<br />
+                            john.doe@gmail.com
+                        </p>
+                        <a href="http://github.com" target="_blank" rel="nofollow">  <i className="bi bi-github mx-2 fs-3 text-secondary "></i></a>
+                        <a href="http://twitter.com" target="_blank" rel="nofollow"><i className="bi bi-twitter  mx-2 fs-3 text-secondary"></i></a>
+                        <a href="http://https://www.linkedin.com/" target="_blank" rel="nofollow"><i className="bi bi-linkedin mx-2 fs-3 text-secondary"></i></a>
+                    </address>
 
-          <div className="col-md-4">
-            <h5>Liens utiles</h5>
-            <ul className="list-unstyled">
-              <li>Accueil</li>
-              <li>Services</li>
-              <li>Portfolio</li>
-              <li>Me contacter</li>
-              <li>Mentions légales</li>
-            </ul>
-          </div>
+                    <div className="col-md-4">
+                        <p className="fs-4 fw-bold">Liens utiles</p>
+                        <ul className="list-unstyled">
+                            <li><NavLink to={"/"}  className="text-light text-decoration-none lien ">Accueil</NavLink></li>
+                            <li><NavLink to={"/Services"}  className="text-light text-decoration-none lien ">Services</NavLink></li>
+                            <li><NavLink to={"/Portfolio"}  className="text-light text-decoration-none lien">Portfolio</NavLink></li>
+                            <li><NavLink to={"/Contact"}  className="text-light text-decoration-none lien">Me contacter</NavLink></li>
+                            <li><NavLink to={"/MentionsLegales"}  className="text-light text-decoration-none lien">Mentions légales</NavLink></li>
+                        </ul>
+                    </div>
 
-          <div className="col-md-4">
-            <h5>Mes dernières réalisations</h5>
-            <ul className="list-unstyled">
-              <li>Fresh Food</li>
-              <li>Restaurant Akira</li>
-              <li>Espace bien-être</li>
-              <li>SEO</li>
-              <li>Création d'une API</li>
-              <li>Maquette d'un site</li>
-            </ul>
-          </div>
+                    <div className="col-md-4">
+                        <p className="fs-4 fw-bold">Mes dernières réalisations</p>
+                        <ul className="list-unstyled">
+                            <li><NavLink to={"/Portfolio"}  className="text-light text-decoration-none lien">Fresh Food</NavLink></li>
+                            <li><NavLink to={"/Portfolio"}  className="text-light text-decoration-none lien">Restaurant Akira</NavLink></li>
+                            <li><NavLink to={"/Portfolio"}  className="text-light text-decoration-none lien">Espace bien-être</NavLink></li>
+                            <li><NavLink to={"/Portfolio"}  className="text-light text-decoration-none lien">SEO</NavLink></li>
+                            <li><NavLink to={"/Portfolio"}  className="text-light text-decoration-none lien">Création d'une API</NavLink></li>
+                            <li><NavLink to={"/Portfolio"}  className="text-light text-decoration-none lien">Maquette d'un site</NavLink></li>
+                        </ul>
+                    </div>
 
-        </div>
-      </div>
-    </footer>
-  );
+                </div>
+            </div>
+        </footer>
+    );
 }
--- src/pages/MentionsLegales.jsx
+++ src/pages/MentionsLegales.jsx
@@ -1,12 +1,13 @@
 import MentionsLegales from "../Assets/Components/AccordionCard"
 const MentionsLegale = () => {
     return (
-        
+        <>
+        <meta name="robots" content="noindex" />
         <main>
        <MentionsLegales/>
         </main>
        
-    
+    </>
     )
 }
 export default MentionsLegale;
